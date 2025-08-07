26d7a6e44f534d0cc0d6dadb37b8870043a40593
Tazio Petersen <petersentazio@gmail.com>
2025-08-07 17:25:42 +0200

    Update: Changed tip.py

--- faces/faces.py
+++
@@ -1,9 +0,0 @@
-def convert(string):
-    string = string.replace(":)", "🙂").replace(":(", "🙁")
-    return string
-
-def main():
-    user_input = input("How are you feeling today? ")
-    print(convert(user_input))
-
-main()
