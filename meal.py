83ae606fd08504a98a2eaf4e1f2579325d0aced6
Tazio Petersen <petersentazio@gmail.com>
2025-08-08 20:37:29 +0200

    Is it time to eat?

---
+++ meal.py
@@ -0,0 +1,22 @@
+def main():
+    user_input = input("What time is it? ")
+    time = convert(user_input)
+
+    if 7.0 <= time <= 8.0:
+        print("breakfast time")
+    elif 12.0 <= time <= 13.0:
+        print("lunch time")
+    elif 18.0 <= time <= 19.0:
+        print("dinner time")
+    else:
+        return None
+
+def convert(time):
+    hours, minutes = time.split(":")
+    hours = float(hours)
+    minutes = float(minutes)
+    return hours + (minutes / 60)
+
+
+if __name__ == "__main__":
+    main()
