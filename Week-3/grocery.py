e8875cc2f5582781c25ae25d78189927ab4d37eb
Tazio Petersen <petersentazio@gmail.com>
2025-08-21 19:14:27 +0200

    This commit completes the grocery.py program. It correctly handles user input, uses a dictionary to count unique items, and prints a final, alphabetically sorted list with each item's count in uppercase.

--- Week-3/grocery.py
+++ Week-3/grocery.py
@@ -1,4 +1,24 @@
 def grocery():
-    list = []
+    grocery_list = {}
+
+    while True:
+
+        try:
+            user = input("")
+            if user in grocery_list:
+                grocery_list[user] += 1
+            else:
+                grocery_list[user] = 1
+
+        except EOFError:
+            break
+
+    print()
+
+    for item, amount in sorted(grocery_list.items()):
+        print(amount, item.upper())
+
+grocery()
+
     
     
