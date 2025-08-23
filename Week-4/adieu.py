35fd7fa26aeda149b434f401619481d1d9a07864
Tazio Petersen <petersentazio@gmail.com>
2025-08-23 16:08:13 +0200

    Added adieu.py

---
+++ Week-4/adieu.py
@@ -0,0 +1,16 @@
+import inflect
+
+p = inflect.engine()
+names = []
+
+while True:
+
+    try:
+        name = input("Name: ")
+        names.append(name)
+
+    except EOFError:
+        break
+
+print()
+print("Adieu, adieu, to", p.join(names))
